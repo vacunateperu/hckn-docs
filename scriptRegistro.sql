7fef718df4cea229616031f95712d273e2c2f4a2
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-02-13 03:10:57 -0500

    Scripts para la BD

---
+++ scriptRegistro.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO public.personal(
+    id_persona,
+    nombres,
+    apellido_paterno,
+    apellido_materno,
+    f_nacimiento,
+    sexo,
+    direccion_domicilio,
+    correo_electronico,
+    documento_identidad,
+    f_registro,
+    id_distrito,
+    code_ocupacion,
+    prob_vulnerabilidad
+) VALUES
+    (1, 'Andrés','Medina','Charcape','20/12/1997','M','URB. San Pedro Mz. B Lt. 19','charcapito@gmail.com','87654321','13/02/2021','021801','NI',''),
+    (2, 'Kenny','Saavedra','Altuna','28/08/1997','M','URB. Jose Carlos Mariategui Mz. E3 Lt. H','kennysaavedraaltuna@outlook.com','72728318','13/02/2021','021809','NI','');
---
+++ scriptTablas.sql
@@ -0,0 +1,22 @@
+
+DROP TABLE public.personal;
+
+CREATE TABLE public.personal
+(
+    id_persona BIGINT NOT NULL,
+    nombres VARCHAR(1024) NOT NULL,
+    apellido_paterno VARCHAR(1024) NOT NULL,
+    apellido_materno VARCHAR(1024) NOT NULL,
+    f_nacimiento DATE NOT NULL,
+    sexo CHAR NOT NULL,
+    direccion_domicilio VARCHAR(1024),
+    correo_electronico VARCHAR(1024),
+    documento_identidad CHAR(8) NOT NULL,
+    f_registro DATE NOT NULL,
+    id_distrito CHAR(6) NOT NULL,
+    code_ocupacion CHAR(8) NOT NULL,
+    prob_vulnerabilidad FLOAT,
+    CONSTRAINT personal_pkey PRIMARY KEY (id_persona)
+);
+
+ALTER TABLE public.personal OWNER to vacunatepeuser;
