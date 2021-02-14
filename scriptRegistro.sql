6e75e758b2069b994b775e429445bff42b9e0134
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-02-14 01:57:25 -0500

    Ajustes 2
    Signed-off-by: Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>

--- scriptRegistro.sql
+++ scriptRegistro.sql
@@ -12,9 +12,10 @@ INSERT INTO public.persona(
     documento_identidad,
     f_registro,
     id_distrito,
-    code_ocupacion
+    code_ocupacion,
+    prob_vulnerabilidad
 ) VALUES
-    ('Andrés','Medina','Charcape','20/12/1997','M','URB. San Pedro Mz. B Lt. 19','charcapito@gmail.com','87654321','13/02/2021','021801','NI'),
-    ('Kenny','Saavedra','Altuna','28/08/1997','M','URB. Jose Carlos Mariategui Mz. E3 Lt. H','kennysaavedraaltuna@outlook.com','72728318','13/02/2021','021809','NI');
+    ('Andrés','Medina','Charcape','20/12/1997','M','URB. San Pedro Mz. B Lt. 19','charcapito@gmail.com','87654321','13/02/2021','021801','NI',0.5123),
+    ('Kenny','Saavedra','Altuna','28/08/1997','M','URB. Jose Carlos Mariategui Mz. E3 Lt. H','kennysaavedraaltuna@outlook.com','72728318','13/02/2021','021809','NI'0.7632);
 
 /*DATOS GENERADOS CON VULNERABILIDAD*/
