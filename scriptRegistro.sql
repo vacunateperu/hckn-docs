b8c37ea3a863d668508c206404054e47795021d7
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-02-13 22:42:36 -0500

    Correcciones a los Script

--- scriptRegistro.sql
+++ scriptRegistro.sql
@@ -1,6 +1,5 @@
 
 INSERT INTO public.personal(
-    id_persona,
     nombres,
     apellido_paterno,
     apellido_materno,
@@ -14,5 +13,5 @@ INSERT INTO public.personal(
     code_ocupacion,
     prob_vulnerabilidad
 ) VALUES
-    (1, 'Andrés','Medina','Charcape','20/12/1997','M','URB. San Pedro Mz. B Lt. 19','charcapito@gmail.com','87654321','13/02/2021','021801','NI',''),
-    (2, 'Kenny','Saavedra','Altuna','28/08/1997','M','URB. Jose Carlos Mariategui Mz. E3 Lt. H','kennysaavedraaltuna@outlook.com','72728318','13/02/2021','021809','NI','');
+    ('Andrés','Medina','Charcape','20/12/1997','M','URB. San Pedro Mz. B Lt. 19','charcapito@gmail.com','87654321','13/02/2021','021801','NI',''),
+    ('Kenny','Saavedra','Altuna','28/08/1997','M','URB. Jose Carlos Mariategui Mz. E3 Lt. H','kennysaavedraaltuna@outlook.com','72728318','13/02/2021','021809','NI','');
--- scriptTablas.sql
+++ scriptTablas.sql
@@ -3,7 +3,7 @@ DROP TABLE public.persona;
 
 CREATE TABLE public.persona
 (
-    id_persona BIGINT NOT NULL,
+    id_persona SERIAL PRIMARY KEY,
     nombres VARCHAR(1024) NOT NULL,
     apellido_paterno VARCHAR(1024) NOT NULL,
     apellido_materno VARCHAR(1024) NOT NULL,
@@ -14,9 +14,8 @@ CREATE TABLE public.persona
     documento_identidad CHAR(8) NOT NULL,
     f_registro DATE NOT NULL,
     id_distrito CHAR(6) NOT NULL,
-    code_ocupacion CHAR(8) NOT NULL,
-    prob_vulnerabilidad FLOAT,
-    CONSTRAINT personal_pkey PRIMARY KEY (id_persona)
+    code_ocupacion CHAR(3) NOT NULL,
+    prob_vulnerabilidad FLOAT
 );
 
 ALTER TABLE public.persona OWNER to vacunatepeuser;
